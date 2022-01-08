404d63fe04945848292fd52c4475f0aff830450a
Jeremy Huynh <jeahuynh18@gmail.com>
2022-01-08 12:12:59 -0800

    added tinymce functionality to edit-post.handlebars

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,10 +1,12 @@
+const id = window.location.toString().split('/')[
+  window.location.toString().split('/').length - 1
+];
+
 async function editFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value.trim();
-    const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
+    
     const response = await fetch(`/api/posts/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
@@ -21,6 +23,22 @@ async function editFormHandler(event) {
       alert(response.statusText);
     }
   }
+  fetch(`/api/posts/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('Successful GET request:', data);
+      tinymce.get("mytextarea").setContent(data.post_description)
+    })
+    .catch((error) => {
+      console.error('Error in POST request:', error);
+    });
   
   document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+
+
   
